edb98d2c4e4a33c99a7522c063c44d0dd90cd3fb
d6mao2019 <65261722+d6mao2019@users.noreply.github.com>
2020-08-03 22:56:10 -0400

    Add files via upload

--- character.cc
+++ character.cc
@@ -1,5 +1,15 @@
 #include "character.h"
+#include <cmath>
+
+void Character::setHP(int new_HP) { HP = new_HP; }
 
 int Character::getHP() const { return HP; }
 int Character::getAtk() const { return Atk; }
 int Character::getDef() const { return Def; }
+
+int Character::calcDamageTo(const Character& other) {
+	double atk = getAtk();
+	double def = other.getDef();
+	int damage = ceil(100 / (100 + def) * atk);
+	return damage;
+}
