a5ae43f6e9be4ee2e1b3185c676d6b756f9abf31
Daniel Mao <d6mao@uwaterloo.ca>
2020-08-13 21:19:30 -0400

    Daniel 8.13. minor changes in potion classes.

--- potion.h
+++ potion.h
@@ -5,41 +5,50 @@
 
 class Potion : public Player
 {
-	int effect;
+	const int effect;
 	int row, col;
 
 protected:
-	std::unique_ptr<Player> player;
+	std::shared_ptr<Player> player;
 
 public:
+	virtual void modifyPlayer() = 0;
 };
 
 class HPBoost : public Potion
 {
+public:
+	virtual void modifyPlayer() override;
 };
 
 class AtkBoost : public Potion
 {
 public:
+	virtual void modifyPlayer() override;
 };
 
 class DefBoost : public Potion
 {
 public:
+	virtual void modifyPlayer() override;
 };
 
 class HPWound : public Potion
 {
+public:
+	virtual void modifyPlayer() override;
 };
 
 class AtkWound : public Potion
 {
 public:
+	virtual void modifyPlayer() override;
 };
 
 class DefWound : public Potion
 {
 public:
+	virtual void modifyPlayer() override;
 };
 
 #endif // !POTION_H
