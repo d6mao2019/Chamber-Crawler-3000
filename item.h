a82f874bdc572675fdcea7d335f58814b71f9e33
Daniel Mao <d6mao@uwaterloo.ca>
2020-08-14 20:00:43 -0400

    Daniel. ifndef ITEM_H

--- item.h
+++ item.h
@@ -1,11 +1,14 @@
+#ifndef ITEM_H
+#define ITEM_H
 class Item
 {
 private:
     int row;
     int col;
     const char type;
+
 public:
-    Item(char type,int row=0, int col=0);
+    Item(char type, int row = 0, int col = 0);
     char getType() const;
     int getRow() const;
     int getCol() const;
@@ -13,3 +16,4 @@ public:
     void setCol(int c);
 };
 
+#endif
